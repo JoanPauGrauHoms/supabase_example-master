01e30569cd9d73691946b6c8fd92d60dc4529d43
JoanPauGrauHoms <jp.grau@sarria.salesians.cat>
2024-01-18 21:40:10 +0100

    final

--- lib/app/modules/add_note/controllers/add_note_controller.dart
+++ lib/app/modules/add_note/controllers/add_note_controller.dart
@@ -1,32 +1,33 @@
-import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/cupertino.dart';  //Biblioteca proporciona widgets per a l'entorn visual iOS
+import 'package:get/get.dart';  //Gestio d'estats i la injeccio de dependencies Flutter
+import 'package:supabase_flutter/supabase_flutter.dart';  //Integracio amb supabase (BaaS)
 
-class AddNoteController extends GetxController {
-  RxBool isLoading = false.obs;
-  RxBool isHidden = true.obs;
-  TextEditingController titleC = TextEditingController();
-  TextEditingController descC = TextEditingController();
-  SupabaseClient client = Supabase.instance.client;
+class AddNoteController extends GetxController {  
+  RxBool isLoading = false.obs; //Indica si s'esta carregant la aplicacio
+  RxBool isHidden = true.obs; //Controlar visibilitat dels elements
+  TextEditingController titleC = TextEditingController(); //Gestio de text al camp de titol de la nota
+  TextEditingController descC = TextEditingController();  //Gestio de descripcio de la nota
+  SupabaseClient client = Supabase.instance.client; //Usarem per interactuar amb el supabase
 
   Future<bool> addNote() async {
-    if (titleC.text.isNotEmpty && descC.text.isNotEmpty) {
-      isLoading.value = true;
+    if (titleC.text.isNotEmpty && descC.text.isNotEmpty) {  //Verifica que l'usuari ha escrit als camps
+      isLoading.value = true; //Passem el valor a true perque es carrega la aplicacio
       List<dynamic> res = await client
           .from("users")
           .select("id")
           .match({"uid": client.auth.currentUser!.id});
       Map<String, dynamic> user = (res).first as Map<String, dynamic>;
-      int id = user["id"]; //get and match user id before insert to db
+      int id = user["id"]; //Obté i fa coincidir la id de l'usuar abans d'insertar a la base de dades
       await client.from("notes").insert({
-        "user_id": id, //insert data with user id as foreign key
-        "title": titleC.text,
-        "description": descC.text,
-        "created_at": DateTime.now().toIso8601String(),
+        "user_id": id, //Inserta dades amb la id de l'usuari com a foreign key
+        "title": titleC.text, //Passem el valor del titol de la nota
+        "description": descC.text,  //Passem el valor de la descripcio de la nota
+        "created_at": DateTime.now().toIso8601String(), //Passem el temps actual per saber quan s'ha creat
       });
-      return true;
+      return true;  //Operacio exitosa
     } else {
-      return false;
+      return false; //Operacio no exitosa
     }
   }
 }
+
--- lib/app/modules/add_note/views/add_note_view.dart
+++ lib/app/modules/add_note/views/add_note_view.dart
@@ -1,59 +1,59 @@
 // ignore_for_file: must_be_immutable
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //Proporciona widgets i eines del UI (user interface)
 
-import 'package:get/get.dart';
-import 'package:supabase_example/app/modules/home/controllers/home_controller.dart';
+import 'package:get/get.dart';  //Gestio d'estats i la injeccio de dependencies Flutter
+import 'package:supabase_example/app/modules/home/controllers/home_controller.dart';  //Importa controlador de home
 
-import '../controllers/add_note_controller.dart';
+import '../controllers/add_note_controller.dart'; //Importacio del controlador del module
 
 class AddNoteView extends GetView<AddNoteController> {
-  HomeController homeC = Get.find();
+  HomeController homeC = Get.find();  //Instancia del controlador de home
 
-  AddNoteView({super.key}); // get controller from another controller
+  AddNoteView({super.key}); //Obtenir el controlador d'un altre controlador
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context) {  //Metode que construeix i retorna la interfaç d'usuari
+    return Scaffold(  //Dins es defineix la estructura (AppBar i el body com a ListView)
         appBar: AppBar(
-          title: const Text('Add Note'),
-          centerTitle: true,
+          title: const Text('Add Note'),  //Titol de l'apartat
+          centerTitle: true,  //Titol centrat
         ),
         body: ListView(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),  //Padding que te l'apartat
           children: [
             TextField(
-              controller: controller.titleC,
+              controller: controller.titleC,  //Passem el controlador
               decoration: const InputDecoration(
-                labelText: "Title",
-                border: OutlineInputBorder(),
+                labelText: "Title", //Text que es mostra dins del camp title
+                border: OutlineInputBorder(), //Estil de la vora
               ),
             ),
             const SizedBox(
-              height: 25,
+              height: 25, //Altura del camp
             ),
             TextField(
-              controller: controller.descC,
+              controller: controller.descC, //Passem el controlador
               decoration: const InputDecoration(
-                labelText: "Description",
-                border: OutlineInputBorder(),
+                labelText: "Description", //Text que es mostra dins del camp description
+                border: OutlineInputBorder(), //Estil de la vora
               ),
             ),
             const SizedBox(
-              height: 20,
+              height: 20, //Altura del camp
             ),
-            Obx(() => ElevatedButton(
-                onPressed: () async {
-                  if (controller.isLoading.isFalse) {
-                    bool res = await controller.addNote();
+            Obx(() => ElevatedButton( //Boto per afegir la nota
+                onPressed: () async { //Al polsar-lo
+                  if (controller.isLoading.isFalse) { //Comprova si esta en estat de carrega
+                    bool res = await controller.addNote();  //Crida per afegir una nota
                     if (res == true) {
-                      await homeC.getAllNotes();
-                      Get.back();
+                      await homeC.getAllNotes();  //Actualitza la llista de notes
+                      Get.back(); //Tanca la pantalla a la que ens trobem
                     }
-                    controller.isLoading.value = false;
+                    controller.isLoading.value = false; //Asignem el valor de carrega a fals
                   }
                 },
                 child: Text(
-                    controller.isLoading.isFalse ? "Add note" : "Loading...")))
+                    controller.isLoading.isFalse ? "Add note" : "Loading..."))) //Text que es mostra quan es carrega
           ],
         ));
   }
